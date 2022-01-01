3a46948bf610d4ea3181524025906a1cb68c5846
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-01 17:17:03 +0400

    Add sidebar component and its style.

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BsFillSunFill, BsFillMoonFill, FaFileInvoiceDollar } from "react-icons/all";
+import "../scss/components/sidebar.scss";
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <main>
+        <div className='logo'>
+          <FaFileInvoiceDollar />
+        </div>
+        <button>{<BsFillMoonFill />}</button>
+      </main>
+      <footer>
+        <img src='https://avatars.githubusercontent.com/u/71825314?v=4' alt='avatar' />
+      </footer>
+    </div>
+  );
+};
+
+export default Sidebar;
