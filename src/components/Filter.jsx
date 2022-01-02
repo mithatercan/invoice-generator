32e01503524df8fb044b47bc6ad9d088ed896345
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-02 22:47:19 +0400

    Add filter dropdown menu and its style.

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,7 +1,30 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { FilterModalContext } from "../context/FilterModalContext";
+import { FiChevronDown, FiChevronUp } from "react-icons/all";
+import "../scss/components/filter.scss";
 const Filter = () => {
-  return <div>Filter</div>;
+  const { isOpen, setIsOpen } = useContext(FilterModalContext);
+  return (
+    <div className='filter'>
+      <button className='display-flex ai-center' onClick={() => setIsOpen(!isOpen)}>
+        Filter by status {isOpen ? <FiChevronUp /> : <FiChevronDown />}
+      </button>
+      <div className={`filter-options ${isOpen && "opened"}`}>
+        <label className='display-flex ai-center'>
+          <input type='checkbox' />
+          <span>Paid</span>
+        </label>
+        <label className='display-flex ai-center'>
+          <input type='checkbox' />
+          <span>Paid</span>
+        </label>
+        <label className='display-flex ai-center'>
+          <input type='checkbox' />
+          <span>Paid</span>
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
