a487f71711d38b189a2fbb4280c8251e59cc06bd
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-03 23:53:09 +0400

    Add icon to invoice button

--- src/components/NewInvoiceBtn.jsx
+++ src/components/NewInvoiceBtn.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { AiFillPlusCircle } from "react-icons/all";
 
 const NewInvoiceBtn = () => {
-  return <button>New Invoice</button>;
+  return (
+    <button className='display-flex ai-center invoice-btn'>
+      <AiFillPlusCircle />
+      New Invoice
+    </button>
+  );
 };
 
 export default NewInvoiceBtn;
