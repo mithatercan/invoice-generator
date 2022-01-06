6f4af906c79feb053d882f5e778a143fabe238c7
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-06 23:15:45 +0400

    Update invoice context

--- src/context/InvoiceContext.js
+++ src/context/InvoiceContext.js
@@ -1,7 +1,32 @@
-import { createContext } from "react";
-
+import { createContext, useState } from "react";
+import html2canvas from "html2canvas";
 export const InvoiceContext = createContext();
 
 export const InvoiceProvider = ({ children }) => {
-  return <InvoiceContext.Provider value='test'> {children}</InvoiceContext.Provider>;
+  const [component, setComponent] = useState(null);
+
+  const downloadInvoice = async () => {
+    const element = component;
+    const canvas = await html2canvas(element);
+
+    const data = canvas.toDataURL("image/jpg");
+    const link = document.createElement("a");
+
+    if (typeof link.download === "string") {
+      link.href = data;
+      link.download = "image.jpg";
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      window.open(data);
+    }
+  };
+
+  return (
+    <InvoiceContext.Provider value={{ setComponent, downloadInvoice }}>
+      {children}
+    </InvoiceContext.Provider>
+  );
 };
