ecb6ed7b8730e0e92908a8479fd7fda5b7a1f67f
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-07 23:27:46 +0400

    Update Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Sidebar from "./Sidebar";
+import InvoiceForm from "./InvoiceForm";
 import "../scss/components/layout.scss";
 
 const Layout = ({ children }) => {
   return (
     <div className='layout display-flex'>
       <Sidebar />
-      <main>{children}</main>
+      <main>
+        {children} <InvoiceForm />
+      </main>
     </div>
   );
 };
