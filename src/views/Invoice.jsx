9b075439c277b9637d527f92dc3f08de060c1fcc
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-11 02:10:30 +0400

    Update views

--- src/views/Invoice.jsx
+++ src/views/Invoice.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { InvoiceContext } from "../context/InvoiceContext";
 import { useParams } from "react-router-dom";
 import InvoiceHeader from "../components/InvoiceHeader";
 import InvoiceCard from "../components/InvoiceCard";
+import data from "../data";
+import "../scss/views/invoice.scss";
 const Invoice = () => {
   const { id } = useParams();
+  const { invoice, setInvoice } = useContext(InvoiceContext);
+
+  useEffect(() => {
+    const invoice = data.find((invoice) => invoice.id === id);
+    setInvoice(invoice);
+  }, [id]);
+
   return (
     <section className='invoice-view'>
-      <InvoiceHeader />
       <InvoiceCard />
     </section>
   );
