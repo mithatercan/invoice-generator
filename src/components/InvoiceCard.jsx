b97be89ddb2a7869408af749908280ac1103c6c3
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-11 02:10:41 +0400

    Update invoice card

--- src/components/InvoiceCard.jsx
+++ src/components/InvoiceCard.jsx
@@ -1,67 +1,57 @@
 import React, { useContext, useEffect, useRef } from "react";
 import { InvoiceContext } from "../context/InvoiceContext";
-import "../scss/components/invoice.scss";
 import InvoiceTable from "./InvoiceTable";
-const InvoiceCard = ({ id }) => {
+import Address from "./Address";
+import "../scss/components/invoice.scss";
+import formatDate from "../helpers/formatDate";
+import InvoiceHeader from "./InvoiceHeader";
+const InvoiceCard = () => {
   const invoiceRef = useRef();
-  const { setComponent } = useContext(InvoiceContext);
+  const { setComponent, invoice } = useContext(InvoiceContext);
+
   useEffect(() => {
     setComponent(invoiceRef.current);
   }, []);
-  return (
-    <div ref={invoiceRef} className='invoice-card'>
-      <header className='display-flex jc-space-between'>
-        <div>
-          <span>#TY9141</span>
-          <small>Landing Page Design</small>
-        </div>
-        <address>
-          <small>
-            19 Union Terrace
-            <br />
-            London
-            <br />
-            E1 3EZ
-            <br />
-            United Kingdom
-          </small>
-        </address>
-      </header>
-      <main className='display-flex jc-space-between'>
-        <div className='invoice-left display-flex jc-space-between fd-column'>
-          <div className='invoice-left__top'>
-            <small> Invoice Date</small>
-            <span>01 Oct 2021</span>
-          </div>
-          <div className='invoice-left__bottom'>
-            <small>Payment Due</small>
-            <span>31 Oct 2021</span>
-          </div>
-        </div>
-        <div className='invoice-mid display-flex fd-column jc-space-between'>
-          <small>Bill To</small>
+
+  return invoice ? (
+    <div>
+      <InvoiceHeader status={invoice.status} />
+      <div ref={invoiceRef} className='invoice-card'>
+        <header className='display-flex jc-space-between'>
           <div>
-            <span>Thomas Wayne</span>
-            <address>
-              <small>
-                19 Union Terrace
-                <br />
-                London
-                <br />
-                E1 3EZ
-                <br />
-                United Kingdom
-              </small>
-            </address>
+            <span>#{invoice.id}</span>
+            <small>{invoice.description}</small>
           </div>
-        </div>
-        <div className='invoice-right'>
-          <small>Sent to</small>
-          <span>thomas@dc.com</span>
-        </div>
-      </main>
-      <InvoiceTable />
+          <Address {...invoice.senderAddress} />
+        </header>
+        <main className='display-flex jc-space-between'>
+          <div className='invoice-left display-flex jc-space-between fd-column'>
+            <div className='invoice-left__top'>
+              <small>Invoice Date</small>
+              <span>{formatDate(invoice.invoiceDate)}</span>
+            </div>
+            <div className='invoice-left__bottom'>
+              <small>Payment Due</small>
+              <span>{formatDate(invoice.paymentDue)}</span>
+            </div>
+          </div>
+          <div className='invoice-mid display-flex fd-column jc-space-between'>
+            <small>Bill To</small>
+            <div>
+              <span>{invoice.clientName}</span>
+              <Address {...invoice.clientAddress} />
+            </div>
+          </div>
+          <div className='invoice-right'>
+            <small>Sent to</small>
+            <span>{invoice.clientEmail}</span>
+          </div>
+        </main>
+        <InvoiceTable currency={invoice.currency} items={invoice.items} />
+      </div>
     </div>
+  ) : (
+    "Loading..."
   );
 };
 
