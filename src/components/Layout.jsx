c8f661e4e7481321b180740374717c180ddb75e9
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-10 23:58:42 +0400

    Update layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import Sidebar from "./Sidebar";
-
+import InvoiceForm from "./InvoiceForm";
+import { InvoiceFormContext } from "../context/InvoiceFormContext";
 import "../scss/components/layout.scss";
 
 const Layout = ({ children }) => {
+  const { isOpened, setIsOpened } = useContext(InvoiceFormContext);
   return (
     <div className='layout display-flex '>
       <Sidebar />
-      <main>{children}</main>
+      <main>
+        {children}
+        <InvoiceForm />
+        <div
+          onClick={() => setIsOpened(false)}
+          className={`back-drop ${isOpened && "opened"}`}
+        ></div>
+      </main>
     </div>
   );
 };
