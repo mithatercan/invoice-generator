4e6ebd26d886b080a83ad08189914f3dd414f74e
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-10 23:58:20 +0400

    Update Layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,18 @@
 import React, { useContext } from "react";
 import { GoMarkGithub, FaFileInvoiceDollar } from "react-icons/all";
-import { InvoiceFormContext } from "../context/InvoiceFormContext";
 import InvoiceForm from "./InvoiceForm";
 import "../scss/components/sidebar.scss";
 const Sidebar = () => {
-  const { isOpened, setIsOpened } = useContext(InvoiceFormContext);
   return (
     <aside className='sidebar display-flex fd-column jc-space-between'>
       <div className='logo'>
         <FaFileInvoiceDollar />
       </div>
-
-      <main className='display-flex fd-column jc-space-between'>
-        <div className='form-wrapper'>
-          <InvoiceForm />
-        </div>
-        <div
-          onClick={() => setIsOpened(false)}
-          className={`${isOpened && "opened"} back-drop`}
-        ></div>
-      </main>
-
+      <main className='display-flex fd-column jc-space-between'></main>
       <footer>
-        <GoMarkGithub />
+        <a href='https://github.com/mithatercann/invoice-generator' target='_blank'>
+          <GoMarkGithub />
+        </a>
       </footer>
     </aside>
   );
