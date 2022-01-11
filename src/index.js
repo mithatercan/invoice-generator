8d2f8b47497cbab5a6772921fd69a58ef4e69905
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-12 00:02:09 +0400

    Add data context provider

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from "react-dom";
 import { InvoiceProvider } from "./context/InvoiceContext";
 import { FilterModalProvider } from "./context/FilterModalContext";
 import { InvoiceFormProvider } from "./context/InvoiceFormContext";
+import { DataProvider } from "./context/DataContext";
 import App from "./App";
 import "./scss/globals/index.scss";
 ReactDOM.render(
   <React.StrictMode>
-    <InvoiceProvider>
-      <FilterModalProvider>
-        <InvoiceFormProvider>
-          <App />
-        </InvoiceFormProvider>
-      </FilterModalProvider>
-    </InvoiceProvider>
+    <DataProvider>
+      <InvoiceProvider>
+        <FilterModalProvider>
+          <InvoiceFormProvider>
+            <App />
+          </InvoiceFormProvider>
+        </FilterModalProvider>
+      </InvoiceProvider>
+    </DataProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
