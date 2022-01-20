9048150f9dad5ed422f7ac895d9e9b35bc3f8904
Mithat Ercan <mithat.ercan@hotmail.com>
2022-01-20 23:35:08 +0400

    Add delete modal and its style.

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import { InvoiceContext } from "../context/InvoiceContext";
+import "../scss/components/delete-modal.scss";
+const DeleteModal = ({ isOpened }) => {
+  const { invoice } = useContext(InvoiceContext);
 
-const DeleteModal = () => {
-  return <div></div>;
+  return (
+    isOpened && (
+      <div className='delete-modal'>
+        <h3>Confirm Deletion</h3>
+        <p>
+          Are you sure you want to delete invoice {invoice && invoice.id} ? This action cannot be
+          undone.
+        </p>
+        <div className='buttons display-flex jc-flex-end ai-center'>
+          <button>Cancel</button>
+          <button>Delete</button>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default DeleteModal;
